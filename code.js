d5acdb8e70b8269556b8f19ab9593dec6ec9bc99
yyk <yyk@mail.ru>
2021-03-21 13:32:28 -0400

    many stars added in order to gain [rot, tr] transformation

--- code.js
+++ code.js
@@ -42,12 +42,14 @@ const N = 5
 
 points = gen_star_points(R1, R2, N)
 p = gen_z_path(points)
-let star1 = add_path(row1, p)
-let star2 = add_path(row1, p)
 let tr = 'translate(100, 60)'
-let rot = 'rotate(45)'
-star1.attr("transform", [rot, tr].join(' '))
-star2.attr("transform", [tr, rot].join(' ')).attr('fill', 'green')
+let stars = [];
+_.range(-10, 10).forEach((i)=>{
+    let rot = 'rotate(' + 10*i + ')'
+    let star = add_path(row1, p).attr("transform", [rot, tr].join(' '))
+    stars.push(star)
+})
+let rot = 'rotate(40)'
 absolute_shift++;
 
 //adding О
