4db6117236e9b2e3362d7fe366552d775cbebb83
yyk <yyk@mail.ru>
2021-03-21 20:23:55 -0400

    almost all shapes placed to proper places

--- code.js
+++ code.js
@@ -3,7 +3,40 @@ const vx1 = -20;
 const vy1 = -20;
 const vx2 = 1000;
 const vy2 = 500;
-
+//::SvgGroup->SvgGroup
+const star = (g)=>{
+    let starPath = gen_z_path(gen_star_points(50, 9, 5))
+    return g.append('path')
+        .attr('d', starPath)
+        .attr('transform', 'rotate(-90)')
+}
+const circle = (g)=>{
+    return g.append('circle')
+        .attr('cx', 0)
+        .attr('yx', 0)
+        .attr('r', 50)
+}
+const rect = (g)=>{
+    return g.append('rect')
+        .attr('x', -50)
+        .attr('y', -50)
+        .attr('width', 100)
+        .attr('height', 100)
+}
+const triangle_norm = ()=>console.log('triangle norm')
+const triangle_small = ()=>console.log('triangle small')
+const triangle_big = ()=>console.log('triangle big')
+let content = [
+    [star, circle, rect],
+    [star, star, star],
+    [star, circle, rect],
+]
+/*let content = [
+    [star, circle, rect],
+    [triangle_norm, triangle_small, triangle_big],
+    [star, circle,rect],
+]
+*/
 var svg = d3.select("#svgcontainer")
     .append("svg")
     .attr("width", 1200)
@@ -23,6 +56,7 @@ _.range(0, 3).forEach(i=>{
     let row = main_g.append('g')
     .attr('class', 'row')
     _.range(0, 3).forEach(j=>{
+        let fn = content[i][j]
         let pathX, pathY, x, y;
         x = j*H;y=i*H;
         pathX = "M" + [x, y].join(',') + "L" +  [x, y + H].join(',')
@@ -35,10 +69,13 @@ _.range(0, 3).forEach(i=>{
         frames = cell_g.append('g')
         frames.append('path').attr('d', pathY).attr('stroke', 'green')
         frames = cell_g.append('g')
-        let startPath = gen_z_path(gen_star_points(20, 9, 5))
-        let trianlgePath
-        frames.append('path').attr('d', startPath)
-        .attr('transform', 'translate(' + (x + R) + ', ' + (y + R) + ') rotate(-90)')
+        let gx = fn(frames)
+        let trs = gx.attr('transform');
+        if (trs) {
+            gx.attr('transform', ' translate(' +[x+R, y+R].join(',')  + ')' + trs)
+        } else {
+            gx.attr('transform', ' translate(' +[x+R, y+R].join(',')  + ')')
+        }
     })
 })
 
