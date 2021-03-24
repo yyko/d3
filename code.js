fba75c3b4636f03da7d84e21ff591b68abbbce86
yyk <yyk@mail.ru>
2021-03-24 14:47:12 -0400

    code.js fixed to fit current lib

--- code.js
+++ code.js
@@ -16,8 +16,6 @@ var main_g = svg.append('g')
     .attr("stroke-width", 1)
 //this could be generated based on data Data Driven Development
 const rowHeight = 100;
-const H = 110
-const R = H/2
 let rows = []
 _.range(0, 3).forEach(i=>{
     let row = main_g.append('g')
@@ -42,40 +40,3 @@ _.range(0, 3).forEach(i=>{
         }
     })
 })
-
-//START adding letters
-//adding  Ж
-
-p = gen_z_path(gen_star_points(R, R2, N))
-let colWidth = rowHeight*1.2
-
-let star1 =  add_path(rows[0], p)
-    //best transformation to move topleft coner as ancor point of the star
-    .attr('transform', 'translate(' + (R + 5) + ',5) rotate(270) scale(1.1, 1.1)')
-
-//adding О
-rows[0].append('circle').attr('cx', 0).attr('cy', 0).attr('r', R)
-.attr('transform', 'translate(' + (colWidth + R) + ', 0)')
-
-//adding П
-L = rowHeight
-rows[0].append('rect') .attr('x', 0) .attr('y', 0) .attr('width', L) .attr('height', L)
-.attr('transform', 'translate(' + 2*colWidth + ', ' + -R + ')')
-//END adding letters
-
-//adding options - two rows for now
-//optionGroup 1
-L = rowHeight/2
-points = [[L/2, 0], [L, L], [0, L]].map(p=>[p[0], p[1]])//triagnle
-p = gen_path(points)
-//add_path(row2, p) .attr('transform', 'translate (0, 0) scale(0.5, 0.5)')
-
-//optionGroup 2
-//let secondOption = add_path(row2, p)
-//secondOption.attr('transform', 'translate (100, 0)')
-
-//optionGroup 3
-let thirdOption = add_path(rows[1], p)
-//secondOption.attr('transform', ' translate (50, 0)')
-
-let secondOptionRow = add_path(rows[2], p)
