f93be1fae50e3ad28f862817516c0dca93c9ed34
yyk <yyk@mail.ru>
2021-03-24 15:30:46 -0400

    ablity to add arbitrary shapes to the grid implemented

--- rules_group_view.js
+++ rules_group_view.js
@@ -1,7 +1,5 @@
 let content = [
-    [star, circle, rect],
-    [triangle_norm, rect, triangle_big],
-    [star, triangle_small, circle],
+    [star, star, encripted(1)]
 ]
 
 var svg = d3.select("#svgcontainer")
@@ -22,20 +20,30 @@ _.range(0, 25).forEach(i=>{
     let row = main_g.append('g')
     .attr('class', 'row')
     _.range(0, 31).forEach(j=>{
+        let cell_g = row.append('g') .attr('class', 'cell')
+        let shape = cell_g.append('g')
         if (show_grid) {
-        let pathX, pathY, x, y, color;
-        color = '#F5F5F5'
+            let pathX, pathY, x, y, color;
+            color = '#F5F5F5'
+            x = j*cellSide;y=i*cellSide;
+            pathX = "M" + [x, y].join(',') + "L" +  [x, y + cellSide].join(',')
+            pathY = "M" + [x, y].join(',') + "L" +  [x + cellSide, y].join(',')
+            pathX_ = "M" + [x, y + cellSide].join(',') + "L" +  [x + cellSide, y + cellSide].join(',')
+            pathY_ = "M" + [x + cellSide, y].join(',') + "L" +  [x + cellSide, y + cellSide].join(',')
+            cell_g.append('path').attr('d', pathX).attr('stroke', color)
+            cell_g.append('path').attr('d', pathY).attr('stroke', color);
+            cell_g.append('path').attr('d', pathX_).attr('stroke', color);
+            cell_g.append('path').attr('d', pathY_).attr('stroke', color);
+        }
+        let fn;
         x = j*cellSide;y=i*cellSide;
-        pathX = "M" + [x, y].join(',') + "L" +  [x, y + cellSide].join(',')
-        pathY = "M" + [x, y].join(',') + "L" +  [x + cellSide, y].join(',')
-        pathX_ = "M" + [x, y + cellSide].join(',') + "L" +  [x + cellSide, y + cellSide].join(',')
-        pathY_ = "M" + [x + cellSide, y].join(',') + "L" +  [x + cellSide, y + cellSide].join(',')
-        let cell_g = row.append('g')
-            .attr('class', 'cell')
-        cell_g.append('path').attr('d', pathX).attr('stroke', color)
-        cell_g.append('path').attr('d', pathY).attr('stroke', color);
-        cell_g.append('path').attr('d', pathX_).attr('stroke', color);
-        cell_g.append('path').attr('d', pathY_).attr('stroke', color);
+        if (content[i]) {fn = content[i][j];}
+        if(fn) {
+            let gx = fn(shape);
+            let trs = gx.attr('transform');//previous transformations
+            let current_transformations = ' translate(' +[x, y].join(',')  + ')';
+            let whole_transformations = trs?current_transformations + trs:current_transformations;
+            gx.attr('transform', whole_transformations)
         }
       })
 })
