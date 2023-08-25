5239ea9a7aae532c51e5bc47d835dcacef2dfaa5
yyk <yyk@mail.ru>
2023-08-24 22:47:58 -0400

    fix: issue with Db is not a counstructor fixed

--- aics.js
+++ aics.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 7777;
-const Db = require('./db.js')
+const { Db } = require('./db.js')
 let db;
 
 app.get("/", async (request, response) => {
